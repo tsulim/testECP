c0efe60c7b003d2ca8cee6244d81bc903c8e005b
XJLim <limxiujia18@gmail.com>
2021-11-23 04:06:34 +0800

    Added js for opening search modal

--- public/js/main.js
+++ public/js/main.js
@@ -59,3 +59,21 @@ jQuery(() => {
         query_bottom_nav();
     });
 })
+
+// For Search Modal ---------------------------------------------
+
+var modal = document.getElementById("searchModal");
+var btn = document.getElementById("openSearchBtn");
+var btnSpan = document.getElementById("openSearch");
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+  btnSpan.text = "close";
+}
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    btnSpan.text = "search";
+  }
+}
