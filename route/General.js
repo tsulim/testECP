d8fcf4224143f333df318502cad4117c98b1b0f9
Alanfy3 <alanthekurayami@gmail.com>
2021-11-29 10:52:40 +0800

    Added Gallery page

--- route/General.js
+++ route/General.js
@@ -1,7 +1,26 @@
+const { raw } = require('body-parser');
+const Images = require('../models/Images');
+
 const router = require('express').Router();
 
 router.get(['/','/home'],(req,res) => {
     res.render('general/home');
+});
+
+router.get(['/gallery'],(req,res) => {
+    Images.findAll({raw: true})
+        .catch(err => console.log(err))
+        .finally(images => {
+            res.render('general/gallery', {
+                images: images
+            });
+        })
+});
+
+// yong yudh: I was thinking of seperating bought artworks and new artworks
+// into gallery and marketplace respectively
+router.get(['/market'],(req,res) => {
+
 });
 
 module.exports = router;
