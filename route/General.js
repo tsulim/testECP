34c0ed65ed570f8407a5d7bd82f347a302627e1e
Ryan Neo <ryrybn@users.noreply.github.com>
2021-12-06 16:27:48 +0800

    Partial completion of first item page

--- route/General.js
+++ route/General.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
 
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
+var s3 = new AWS.S3();
+
+
 router.get(['/','/home'],(req,res) => {
     res.render('general/home');
 });
 
+router.get(['/','/item'],(req,res) => {
+
+    var getParams = {
+        Bucket: 'artionstoragebucket',
+        Key: 'kigNFT.png'
+    }
+
+    var getParams = {Bucket: 'artionstoragebucket', Key: 'kigNFT.png'};
+    var picURL = s3.getObject();
+    console.log(picURL);
+    // ('getObject', getParams);
+
+    res.render('general/item', picURL);
+});
+
 module.exports = router;
+
+
