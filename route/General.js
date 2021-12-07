24d6f16c1ac1aa577b338289749193d862c25b69
XJLim <limxiujia18@gmail.com>
2021-12-08 02:40:09 +0800

    Create NFT Page - left upload image

--- route/General.js
+++ route/General.js
@@ -4,4 +4,12 @@ router.get(['/','/home'],(req,res) => {
     res.render('general/home');
 });
 
+router.get(['/create'],(req,res) => {
+    res.render('general/createnft');
+});
+
+router.post(['/createNFT'], (req, res) => {
+    res.render('general/createCollab');
+});
+
 module.exports = router;
